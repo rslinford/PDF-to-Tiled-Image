0a9f6af21ba63148ba0add0ce55dd54c16ae0d95
rslinford <scott@linfords.com>
2016-11-21 15:57:21 -0800

    Initial commit. CLI template functionality only.

---
+++ PDF-to-Tiled-Image/PDF-to-Tiled-Image/PDF_to_Tiled_Image.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import traceback
+import json
+from PIL import Image
+import piexif
+from datetime import datetime
+
+
+"""
+Do all the stuff: 
+   1) create target dir based on Date Taken
+   2) rename photos based on Date Taken
+   3) move photos to newly created target dir
+   4) set metadata in photos including Date Taken
+"""
+def create_tiled_image(config):
+   pass
+
+################# BEGIN Main Template With Config ######################
+
+def print_config_file(config):
+   print('Config file located at:\n\t%s\nPoint "pdf_source_file" path to your files. Use fully qualified path or relative path. Current working directory:\n\t%s' \
+      % (config['config_file_name'], os.getcwd()))
+   print('Current config contents:')
+   with open(config['config_file_name'], 'r') as f:
+      for line in f:
+         print(line)
+
+def save_config(config):
+   with open(config['config_file_name'], 'w') as f:
+      json.dump(config, f)
+
+def normalize_config(config):
+   config['config_file_name'] = config.get('config_file_name', r'PDF-to-Tiled-Image_settings.json')
+   config['pdf_source_file'] = config.get('pdf_source_file', 'my_pdf_file_with_images.pdf')
+
+def create_default_config(config_file_name):
+   config = {'config_file_name':config_file_name}
+   normalize_config(config)
+   save_config(config)
+   print('New config file created.')
+   print_config_file(config)
+
+def load_config(config_file_name):
+   with open(config_file_name, 'r') as f:
+      config = json.load(f)
+   config['config_file_name'] = config_file_name
+   return config
+
+def main():
+   config_file_name = r'PDF-to-Tiled-Image_settings.json'
+
+   try:
+      config = load_config(config_file_name)
+      if not os.path.isfile(config['pdf_source_file']):
+         print('pdf_source_file is not a file:\n\t%s' % config['pdf_source_file'])
+         print_config_file(config)
+         return 1
+      normalize_config(config)
+   except (FileNotFoundError):
+      create_default_config(config_file_name)
+      raise
+
+   create_tiled_image(config)
+
+if __name__ == '__main__':
+   try:
+      main()
+      print('\n[Normal Exit]')
+   except (KeyboardInterrupt):
+      print('\n[User Exit]')
+   except (SystemExit):
+      print('\n[System Exit]')
