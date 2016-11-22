16b83732da03e41d700ac5dc1bfad05a1ea7417f
rslinford <scott@linfords.com>
2016-11-21 21:42:23 -0800

    create_collage in progress: creates a composite image but crudely arranged

--- PDF-to-Tiled-Image/PDF-to-Tiled-Image/PDF_to_Tiled_Image.py
+++ PDF-to-Tiled-Image/PDF-to-Tiled-Image/PDF_to_Tiled_Image.py
@@ -8,37 +8,69 @@ from datetime import datetime
 import PyPDF2
 import tempfile
 
-def extract_images_from_page(config, page_number, working_dir, xObject, depth = 0):
-    xObject = xObject['/Resources']['/XObject'].getObject()
-    image_number = 0
-    for obj in xObject:
+def extract_images_from_page(config, page_number, working_dir, xObject, depth=0):
+   xObject = xObject['/Resources']['/XObject'].getObject()
+   image_number = 0
+   for obj in xObject:
 
-        if xObject[obj]['/Subtype'] == '/Image':
-            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-            data = xObject[obj]._data
-            if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
-                mode = "RGB"
-            else:
-                mode = "P"
+      if xObject[obj]['/Subtype'] == '/Image':
+         size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+         data = xObject[obj]._data
+         if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+            mode = "RGB"
+         else:
+            mode = "P"
 
-            imagepath = os.path.join(working_dir, 'image-p%03d-d%03d-i%03d' %(page_number, depth, image_number))
+         imagepath = os.path.join(working_dir, 'image-p%03d-d%03d-i%03d' % (page_number, depth, image_number))
 
-            if xObject[obj]['/Filter'] == '/FlateDecode':
-                img = Image.frombytes(mode, size, data)
-                img.save(imagepath + ".png")
-                image_number += 1
-            elif xObject[obj]['/Filter'] == '/DCTDecode':
-                img = open(imagepath + ".jpg", "wb")
-                img.write(data)
-                img.close()
-                image_number += 1
-            elif xObject[obj]['/Filter'] == '/JPXDecode':
-                img = open(imagepath + ".jp2", "wb")
-                img.write(data)
-                img.close()
-                image_number += 1
-        else:
-            extract_images_from_page(config, page_number, working_dir, xObject[obj], depth = depth + 1)
+         if xObject[obj]['/Filter'] == '/FlateDecode':
+            img = Image.frombytes(mode, size, data)
+            img.save(imagepath + ".png")
+            image_number += 1
+         elif xObject[obj]['/Filter'] == '/DCTDecode':
+            img = open(imagepath + ".jpg", "wb")
+            img.write(data)
+            img.close()
+            image_number += 1
+         elif xObject[obj]['/Filter'] == '/JPXDecode':
+            img = open(imagepath + ".jp2", "wb")
+            img.write(data)
+            img.close()
+            image_number += 1
+      else:
+         extract_images_from_page(config, page_number, working_dir, xObject[obj], depth = depth + 1)
+
+def create_collage(width, height, listofimages):
+   cols = 4
+   rows = 2
+   thumbnail_width = width//cols
+   thumbnail_height = height//rows
+   size = thumbnail_width, thumbnail_height
+   new_im = Image.new('RGB', (width, height))
+   ims = []
+   for p in listofimages:
+      im = Image.open(p)
+      im.thumbnail(size)
+      ims.append(im)
+   i = 0
+   x = 0
+   y = 0
+   for col in range(cols):
+      for row in range(rows):
+         print(i, x, y)
+         new_im.paste(ims[i], (x, y))
+         i += 1
+         y += thumbnail_height
+      x += thumbnail_width
+      y = 0
+   new_im.save("Collage.jpg")
+
+def tile_images(config, working_dir):
+   filelist = []
+   for filename in os.listdir(working_dir):
+       filepath = os.path.join(working_dir, filename)
+       filelist.append(filepath)
+   create_collage(1920, 1080, filelist)
 
 def clean_up_working_files(working_dir):
    for filename in os.listdir(working_dir):
@@ -64,6 +96,7 @@ Do all the stuff:
 def create_tiled_image(config):
    working_dir = tempfile.mkdtemp(prefix = 'pdf-to-tiled_')
    extract_images_from_all_pages(config, working_dir)
+   tile_images(config, working_dir)
    clean_up_working_files(working_dir)
 
 ################# BEGIN Main Template With Config ######################
