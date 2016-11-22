fc6fc372a01184d68ee323c59310444d6d92ad83
rslinford <scott@linfords.com>
2016-11-21 20:47:05 -0800

    Extracts all images from a PDF. TODO: create tiled image

--- PDF-to-Tiled-Image/PDF-to-Tiled-Image/PDF_to_Tiled_Image.py
+++ PDF-to-Tiled-Image/PDF-to-Tiled-Image/PDF_to_Tiled_Image.py
@@ -5,16 +5,66 @@ import json
 from PIL import Image
 import piexif
 from datetime import datetime
+import PyPDF2
+import tempfile
 
+def extract_images_from_page(config, page_number, working_dir, xObject, depth = 0):
+    xObject = xObject['/Resources']['/XObject'].getObject()
+    image_number = 0
+    for obj in xObject:
+
+        if xObject[obj]['/Subtype'] == '/Image':
+            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+            data = xObject[obj]._data
+            if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                mode = "RGB"
+            else:
+                mode = "P"
+
+            imagepath = os.path.join(working_dir, 'image-p%03d-d%03d-i%03d' %(page_number, depth, image_number))
+
+            if xObject[obj]['/Filter'] == '/FlateDecode':
+                img = Image.frombytes(mode, size, data)
+                img.save(imagepath + ".png")
+                image_number += 1
+            elif xObject[obj]['/Filter'] == '/DCTDecode':
+                img = open(imagepath + ".jpg", "wb")
+                img.write(data)
+                img.close()
+                image_number += 1
+            elif xObject[obj]['/Filter'] == '/JPXDecode':
+                img = open(imagepath + ".jp2", "wb")
+                img.write(data)
+                img.close()
+                image_number += 1
+        else:
+            extract_images_from_page(config, page_number, working_dir, xObject[obj], depth = depth + 1)
+
+def clean_up_working_files(working_dir):
+   for filename in os.listdir(working_dir):
+       filepath = os.path.join(working_dir, filename)
+       os.remove(filepath)
+   os.removedirs(working_dir)
+
+def extract_images_from_all_pages(config, working_dir):
+   pdf_object = PyPDF2.PdfFileReader(open(config['pdf_source_file'], 'rb'))
+   page_count = pdf_object.getNumPages()
+   print('%d pages in %s' % (page_count, config['pdf_source_file']))
+   for p in range(page_count):
+      page_object = pdf_object.getPage(p)
+      extract_images_from_page(config, p, working_dir, page_object)
 
 """
 Do all the stuff: 
    1) extract images from pdf
    2) tile images
    3) save tiled image
+   4) clean up working files
 """
 def create_tiled_image(config):
-   pass
+   working_dir = tempfile.mkdtemp(prefix = 'pdf-to-tiled_')
+   extract_images_from_all_pages(config, working_dir)
+   clean_up_working_files(working_dir)
 
 ################# BEGIN Main Template With Config ######################
 
